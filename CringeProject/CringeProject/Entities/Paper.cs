46e53203d6c0b95cfd4e3c26eae8738ad001214c
Sergiu Chis <sergiuchis2k@gmail.com>
2021-04-06 21:01:27 +0300

    Nothing

---
+++ CringeProject/CringeProject/Entities/Paper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CringeProject.Entities
+{
+    class Paper
+    {
+        private string Abstract { get; set; }
+        private File Body;
+    }
+}
---
+++ CringeProject/CringeProject/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CringeProject.Entities.Users
+{
+    class User
+    {
+        private string UserName { get; set; }
+        private string Password { get; set; }
+        private string Type { get; set; }
+
+        public User(string userName, string password, string type)
+        {
+            UserName = userName ?? throw new ArgumentNullException(nameof(userName));
+            Password = password ?? throw new ArgumentNullException(nameof(password));
+            Type = type ?? throw new ArgumentNullException(nameof(type));
+        }
+    }
+}
