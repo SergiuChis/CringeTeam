aea3b80d406845e912ecc9b603e25d3b1176af41
Dragos Cirtorosan <56576401+qqrtos@users.noreply.github.com>
2021-05-05 01:03:28 +0300

    Refactoring.

--- CringeProject/CringeProject/Entities/User.cs
+++ CringeProject/CringeProject/Entities/User.cs
@@ -14,6 +14,7 @@ namespace CringeProject.Entities
         public string UserName { get; set; }
         public string Password { get; set; }
 
+        public User() {}
         public User(string userName, string password)
         {
             UserName = userName ?? throw new ArgumentNullException(nameof(userName));
