31e81b8e8067bc7d4eea9f66af66f4e4c3fc4c3f
Sergiu Chis <sergiuchis2k@gmail.com>
2021-05-22 16:24:01 +0300

    Listener window

---
+++ CringeProject/CringeProject/Services/ListenerService.cs
@@ -0,0 +1,32 @@
+﻿using CringeProject.Entities;
+using CringeProject.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CringeProject.Services
+{
+    public class ListenerService
+    {
+        private readonly RepositoryContext _repository;
+
+        public ListenerService(RepositoryContext repositoryContext)
+        {
+            _repository = repositoryContext;
+        }
+
+        public string GetRoomName(Participation participation)
+        {
+            var roomName = _repository.Sections.Where(s => s.Id == participation.SectionId).Select(s => s.Room);
+            return roomName.GetEnumerator().Current;
+        }
+
+        public int GetAvailablePlaces(Participation participation)
+        {
+            var availablePlaces = _repository.Sections.Where(s => s.Id == participation.SectionId).Select(s => s.AvailablePlaces);
+            return availablePlaces.GetEnumerator().Current;
+        }
+    }
+}
